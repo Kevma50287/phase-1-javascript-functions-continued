34730c740845b5d3c7c4b53da3141d6d5ed757e7
Kevin <magavakevin10@gmail.com>
2022-07-18 17:59:18 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(action = 'roller-skate') {
+    return `This Saturday, I want to ${action}!`;
+}
+
+function mondayWork(action = 'go to the office') {
+    return `This Monday, I will ${action}.`;
+}
+
+function wrapAdjective(symb) {
+    return function (a = 'special'){
+        return `You are ${symb}${a}${symb}!`;
+    }
+}
+
